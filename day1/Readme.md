cb61de27e932ab67b089f41f8bb1a518b15acb95
Nestor Gonzalez <nestor.ge222@gmail.com>
2022-01-04 22:31:17 -0600

    day1: variables and types

---
+++ day1/Readme.md
@@ -0,0 +1,5 @@
+#Rules for variables
+1 make redeable code
+2 have to init whit a leter
+3 don't use name of special worls on variables
+4 
---
+++ day1/proyect.py
@@ -0,0 +1,7 @@
+print("""
+Welcome to Band Generator \n
+please enter the city you grew up ?
+""")
+city = input()
+pet = input('enter the name of you pet ')
+print ('name: '+ city + " " + pet)

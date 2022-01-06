7ebf350f6a1cf7976461ce802e813e3daa88ce44
Nestor Gonzalez <nestor.ge222@gmail.com>
2022-01-05 20:35:11 -0600

    string con formatos

---
+++ day2/strig_format.py
@@ -0,0 +1,17 @@
+print(round(8/3, 2))#para ingresar la cantidad de numeros al final 
+print((8//2))#esto da un numero entero 
+#auto sumas 
+result =4 
+print(result)
+result /=2
+print(result)
+result+=2
+print(result)
+
+score =0 
+heigh = 1.8
+isWinning =True
+
+#f-string 
+print(f'you score is {score}, yout heigh is {heigh}')
+

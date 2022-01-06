88f62f193eb026bcb191683a72bbef8f6acec4ea
Nestor Gonzalez <nestor.ge222@gmail.com>
2022-01-05 20:43:01 -0600

    challenge3 terminado

---
+++ day2/challenge3.py
@@ -0,0 +1,14 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+years_left = 90-int(age)
+total_days = years_left * 365
+total_wees = years_left * 52
+total_months = years_left* 12 
+
+print(f'You have {total_days} days, {total_wees} weeks, and {total_months} months left.')
+
+

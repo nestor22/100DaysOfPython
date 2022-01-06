30f37a2953a602870d182d564c9b92c42b65514f
Nestor Gonzalez <nestor.ge222@gmail.com>
2022-01-05 18:55:07 -0600

    day2: tipos de datos primitivos

---
+++ day2/exercixe1.py
@@ -0,0 +1,33 @@
+#datatypes 
+#string
+#number
+#floats
+#booleans
+
+
+#string 
+name = 'hello'
+#indexables 
+print(name[0])
+#a esto se le llama subscript 
+
+number = '123'
+number2 = '345'
+
+print(number+number2)
+
+#number = integer 
+number_int = 123
+number_int2 = 345
+print(number_int + number_int2)
+
+#float 
+
+float_number = 1.23
+#boolean 
+
+is_true = True
+is_false = False 
+
+print(is_true)
+

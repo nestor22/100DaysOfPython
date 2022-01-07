b2b524412721d8fd6783b09edfe3a9154f7b9e27
Nestor Gonzalez <nestor.ge222@gmail.com>
2022-01-06 21:21:21 -0600

    ejercicio de pizza

--- day3/nestedif.py
+++ day3/nestedif.py
@@ -5,11 +5,21 @@ if height>120:
     print('you can ride the rollercoster')
     age = int(input('enter your age?'))
     if age < 12:
+        bill = 5
         print('you must pay 5 dollars')
     elif age <= 18: 
+        bill =12
         print('yout must pay 12 dollars')
     else:
+        bill=17
         print('you must pay 17 dollars')
+
+    what_photo = input('Do you want a photo taken? Y or N')
+    if what_photo == 'Y':
+        bill += 3
+
+    print(f'you bill is {bill}')
+
 else:
     print('Sorry, you have to grow taller before you can ride ')
 

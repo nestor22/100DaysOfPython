f1127924d7e5b6a742854b62a7173c7b9e46dce3
Nestor Gonzalez <nestor.ge222@gmail.com>
2022-01-10 20:56:06 -0600

    day4 comparerismo

---
+++ day4/random_numbers.py
@@ -0,0 +1,12 @@
+import random
+import ex_module
+
+random_integer = random.randrange(1, 10)
+print(random_integer)
+
+
+print(ex_module.pi)
+
+randomFloat = random.random() * 5
+
+love_score= random.randint*1, 100

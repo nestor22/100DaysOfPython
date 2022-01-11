b5535f53d1cd168301e728b94a64cec1ddf63394
Nestor Gonzalez <nestor.ge222@gmail.com>
2022-01-10 21:18:04 -0600

    day4 random choice for a payment

---
+++ day4/random_meal.py
@@ -0,0 +1,16 @@
+import random
+# 🚨 Don't change the code below 👇
+test_seed = int(input("Create a seed number: "))
+random.seed(test_seed)
+
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+participants = len(names)
+winer = random.randint(0, participants - 1)
+
+print(f'{names[winer]} is going to buy the meal today!')
+

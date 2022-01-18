873858be7c37330a32acd41ce2a1cd5380f3b475
Nestor Gonzalez <nestor.ge222@gmail.com>
2022-01-17 21:20:30 -0600

    while loop

---
+++ day6/reborgworld.py
@@ -0,0 +1,13 @@
+#codigo para reeborg's world 
+#como el juego de la tortuga
+def turn_around():
+    turn_left()
+    turn_left()
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+
---
+++ day6/while_loop.py
@@ -0,0 +1,22 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+jumps = 0 
+
+while jumps < 7:
+    jump()
+    jumps += 1
+
+

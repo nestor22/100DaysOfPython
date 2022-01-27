dc15e0062681ba403569ede19c41873c4e40aae0
Nestor Gonzalez <nestor.ge222@gmail.com>
2022-01-26 21:41:37 -0600

    day7 tercera parte del juego de aorcado

--- day7/hangmanGame.py
+++ day7/hangmanGame.py
@@ -7,13 +7,17 @@ world_showed = []
 for letter in rando_world:
     world_showed.append("_")
 
-choisen_world = input('guess a letter: ').lower()
-
-print(f'hint the solution is {rando_world}')
 print(world_showed)
+end_of_game=False
+while not end_of_game:
+    choisen_world = input('guess a letter: ').lower()
+    for pos in range(len(rando_world)):
+        if rando_world[pos] == choisen_world:
+            world_showed[pos]=choisen_world
 
-for pos in range(len(rando_world)):
-    if rando_world[pos] == choisen_world:
-        world_showed[pos]=choisen_world
 
-print(world_showed)
+    print(world_showed)
+    if "_" not in world_showed:
+        end_of_game = True
+
+print('end of the game')

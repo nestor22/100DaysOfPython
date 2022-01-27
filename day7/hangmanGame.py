123cb0b7f26cabc4e4ac043761dd61bf4326677a
Nestor Gonzalez <nestor.ge222@gmail.com>
2022-01-26 21:50:38 -0600

    day7 cuarta parte del juego de ahorcado

--- day7/hangmanGame.py
+++ day7/hangmanGame.py
@@ -7,17 +7,28 @@ world_showed = []
 for letter in rando_world:
     world_showed.append("_")
 
+lives = len(world_showed)
 print(world_showed)
 end_of_game=False
 while not end_of_game:
     choisen_world = input('guess a letter: ').lower()
+    print(lives)
     for pos in range(len(rando_world)):
         if rando_world[pos] == choisen_world:
             world_showed[pos]=choisen_world
+
+    if choisen_world not in rando_world:
+        lives-=1
+
+    if lives == 0:
+        end_of_game = True
+        print('you lose')
 
 
     print(world_showed)
     if "_" not in world_showed:
         end_of_game = True
+        print('you win')
+
 
 print('end of the game')

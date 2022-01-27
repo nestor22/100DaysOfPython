cb6b35750d6689397f031e2db93c7a6109f07b69
Nestor Gonzalez <nestor.ge222@gmail.com>
2022-01-26 21:27:17 -0600

    day7 segunda parte del juego de ahorcado

--- day7/hangmanGame.py
+++ day7/hangmanGame.py
@@ -3,11 +3,17 @@ import random
 word_list = ['mouse', 'cat', 'dog', 'came', 'house']
 
 rando_world = random.choice(word_list)
+world_showed = []
+for letter in rando_world:
+    world_showed.append("_")
 
 choisen_world = input('guess a letter: ').lower()
 
-for letter in rando_world:
-    if letter == choisen_world:
-        print('right')
-    else:
-        print('fail')
+print(f'hint the solution is {rando_world}')
+print(world_showed)
+
+for pos in range(len(rando_world)):
+    if rando_world[pos] == choisen_world:
+        world_showed[pos]=choisen_world
+
+print(world_showed)
